4c0e91e58dfe9d1a96210ca7c3b085b7d755e9ac
daxp472 <daxpatel.cg@gmail.com>
2025-04-11 10:03:31 +0530

    node task to completed

---
+++ Node/task/server2.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+const users = [
+    { id: 1, name: 'Dax Patel', email: 'dax@example.com' },
+    { id: 2, name: 'Dev Patel', email: 'dev@example.com' },
+    { id: 3, name: 'Kalp Patel', email: 'kalp@example.com' }
+];
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
